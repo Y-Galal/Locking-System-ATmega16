ae2ff3b2fcba677225051d0cf65cd9ceabf1d81f
Youssef Galal <62811605+Y-Galal@users.noreply.github.com>
2020-05-03 03:06:20 +0200

    editing makefile.mk

--- Processing_Unit/makefile.mk
+++ Processing_Unit/makefile.mk
@@ -5,7 +5,7 @@ OBJCOPY=avr-objcopy
 ELF=elf_out
 MAP=map_out
 SIZE=avr-size
-CFLAGS=-Wl,-Map,out/${MAP}.map -mmcu=${MCU} -DF_CPU=${F_CPU} -Iinc -O1
+CFLAGS=-Wl,-Map,out/${MAP}.map -mmcu=${MCU} -DF_CPU=${F_CPU} -Iinc -O1 -std=gnu99
 TARGET=hex_out
 SRCS=*.c
 
